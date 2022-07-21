f979899a896c27e7a6166e8c9054f1bd35d5e4a1
Aditya Kishore <aditya20kishore@gmail.com>
2022-07-21 22:03:19 +0530

    connected to frontend

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,83 +1,71 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+
+import { AppWrap } from '../../wrapper';
+import { images } from '../../constants';
 import './Header.scss';
-import { motion } from 'framer-motion'
-import { images } from '../../constants'
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+};
 
-const scaleVariants={
-  whileInView:{
-    scale: [0,1],
-    opacity:[0,1],
-    transition:{
-      duration:1,
-      ease:'easeInOut'
-    }
-  }
-}
-
-const Header = () => {
-  return (
-    <div id='home' className='app__header app__flex'>
-      <motion.div
-        whileInView={{x: [-100,0], opacity: [0,1]}}
-        transition={{duration: 0.5}}
-        className="app__header-info"
-        >
-          <div className='app__header-badge'>
-            <div className='badge-cmp app__flex'>
-              <span>👋</span>
-              <div style={{marginLeft: 20}}>
-                <p className='p-text'>Hello, I am</p>
-                <h1 className='head-text'>Aditya</h1>
-              </div>
-
-            </div>
-
-            <div className="tag-cmp app__flex">
-            <p className='p-text'>MSCS (AI)</p>
-            <p className='p-text'>USC</p>
-            </div>
+const Header = () => (
+  <div className="app__header app__flex">
+    <motion.div
+      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+      transition={{ duration: 0.5 }}
+      className="app__header-info"
+    >
+      <div className="app__header-badge">
+        <div className="badge-cmp app__flex">
+          <span>👋</span>
+          <div style={{ marginLeft: 20 }}>
+            <p className="p-text">Hello, I am</p>
+            <h1 className="head-text">Micael</h1>
           </div>
+        </div>
 
+        <div className="tag-cmp app__flex">
+          <p className="p-text">Web Developer</p>
+          <p className="p-text">Freelancer</p>
+        </div>
+      </div>
+    </motion.div>
 
-      </motion.div>
-
-      <motion.div
-      whileInView={{opacity: [0,1]}}
-      transition={{duration: 0.5, delayChildren: 0.5}}
+    <motion.div
+      whileInView={{ opacity: [0, 1] }}
+      transition={{ duration: 0.5, delayChildren: 0.5 }}
       className="app__header-img"
-      
-      >
-        <img src={images.profile} alt="profile_bg"/>
-        <motion.img
-        whileInView={{opacity: [0,1]}}
-        transition={{duration: 1, ease:'easeInOut'}}
+    >
+      <img src={images.profile} alt="profile_bg" />
+      <motion.img
+        whileInView={{ scale: [0, 1] }}
+        transition={{ duration: 1, ease: 'easeInOut' }}
         src={images.circle}
-        alt="Profile_circle"
-        className='overlay_circle'
-        >
+        alt="profile_circle"
+        className="overlay_circle"
+      />
+    </motion.div>
 
-        </motion.img>
-
-
-      </motion.div>
-
-      <motion.div
-        variant={scaleVariants}
-        whileInView={scaleVariants.whileInView}
-        className="app__header-circles"
-      >
-
-      {[images.flutter, images.redux, images.sass].map((circle, index)=>(
-
-          <div className='circle-cmp app__flex' key={`circle-${index}`}>
-          <img src ={circle} alt='circle'/>
-          </div>
+    <motion.div
+      variants={scaleVariants}
+      whileInView={scaleVariants.whileInView}
+      className="app__header-circles"
+    >
+      {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        <div className="circle-cmp app__flex" key={`circle-${index}`}>
+          <img src={circle} alt="profile_bg" />
+        </div>
       ))}
-      </motion.div>
+    </motion.div>
+  </div>
+);
 
-    </div>
-  )
-}
-
-export default Header
+export default AppWrap(Header, 'home');
