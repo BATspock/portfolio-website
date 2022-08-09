5b940a4c48562a1c793d04821f75bd74b070a8dc
Aditya Kishore <aditya20kishore@gmail.com>
2022-08-09 13:31:53 +0530

    constraints folders issue with calling delted resume resolved

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -35,8 +35,8 @@ import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
-import resumeML from '../assets/data/Aditya Kishore Resume ML USC.pdf';
-import resumeSDE from '../assets/data/Aditya Kishore Resume SDE USC.pdf';
+// import resumeML from '../assets/data/Aditya Kishore Resume ML USC.pdf';
+// import resumeSDE from '../assets/data/Aditya Kishore Resume SDE USC.pdf';
 import resume from '../assets/data/Aditya Kishore Resume.pdf';
 export default {
   email,
@@ -73,7 +73,7 @@ export default {
   nb,
   skype,
   spotify,
-  resumeML,
-  resumeSDE,
+  // resumeML,
+  // resumeSDE,
   resume
 };
